3ca966602f9de8c72a9b9044714ec903ad01e289
nitzan-massad <nitzanmassad@gmail.com>
2023-07-16 15:58:29 +0300

    add 404

--- app.js
+++ app.js
@@ -26,6 +26,11 @@ app.use(listsRouters)
 app.use(itemRouters)
 
 
+app.use((req, res, next) => {
+  const error = new HttpError('Could not find this route.', 404);
+  throw error;
+});
+
 
 app.use((error, req, res, next) => {
   if (res.headerSent) {
