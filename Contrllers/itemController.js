4002cb115bab055d7ff3118a6f84ddae5d167cc6
nitzan-massad <nitzanmassad@gmail.com>
2023-12-16 17:09:42 +0200

    Revert "and the ability to reorder the items in the list"
    This reverts commit f5dd516a11204f3ef9e227ad723d42ed9caa2b9f.

--- Contrllers/itemController.js
+++ Contrllers/itemController.js
@@ -1,11 +1,10 @@
 const { validationResult } = require('express-validator')
 const mongoose = require('mongoose')
-const { ObjectId } = require('mongoose').Types
-const { Mutex } = require('async-mutex')
 
+const List = require('../models/ListModule')
+const ListItem = require('../models/ListItemModule')
 const HttpError = require('../models/http-error')
 const authorizationUtils = require('../Utils/authorizationUtils')
-const mutex_markAnItemAsCompletedOrNot = new Mutex()
 
 exports.markAnItemAsCompletedOrNot = async (req, res, next) => {
   const errors = validationResult(req)
@@ -16,11 +15,8 @@ exports.markAnItemAsCompletedOrNot = async (req, res, next) => {
   }
   const { itemId, checkUncheckBool } = req.body
 
-  let item = await authorizationUtils.itemPopulateAndAutorizationUse(
-    itemId,
-    req.userData.userId,
-    next
-  )
+  let item =await authorizationUtils.itemPopulateAndAutorizationUse(itemId,req.userData.userId,next)
+ 
   if (checkUncheckBool) {
     item.completedDate = Date.now()
   } else {
@@ -28,40 +24,17 @@ exports.markAnItemAsCompletedOrNot = async (req, res, next) => {
   }
   item.isDone = checkUncheckBool ?? false
 
-  const targetItemId = new ObjectId(itemId)
-  const release = await mutex_markAnItemAsCompletedOrNot.acquire(item.list)
-  let list = await authorizationUtils.listPopulateItemsAndAutorizationUse(
-    item.list,
-    req.userData.userId,
-    next
-  )
-
-  const newListOrder = Array.from(list.items)
-  const itemsPositionStart = newListOrder.findIndex(item => {
-    return item._id.equals(targetItemId)
-  })
-  const [removed] = newListOrder.splice(itemsPositionStart, 1)
-  let itemsPositionEnd = newListOrder.findIndex(item => {
-    return item.isDone === true
-  })
-  itemsPositionEnd =
-    itemsPositionEnd === -1 ? newListOrder.length : itemsPositionEnd
-  newListOrder.splice(itemsPositionEnd, 0, removed)
-  list.items = newListOrder
-
   try {
     const sess = await mongoose.startSession()
     sess.startTransaction()
     await item.save({ session: sess })
-    await list.save({ session: sess })
     await sess.commitTransaction()
   } catch (err) {
     console.log(err)
     const error = new HttpError('updating item failed, please try again.', 500)
     return next(error)
-  } finally {
-    release()
   }
+
   const itemResponse = item.toObject()
   delete itemResponse.list
   res.status(201).json({ itemResponse })
@@ -76,11 +49,7 @@ exports.editItemTitle = async (req, res, next) => {
   }
   const { itemId, newItemTitle } = req.body
 
-  let item = await authorizationUtils.itemPopulateAndAutorizationUse(
-    itemId,
-    req.userData.userId,
-    next
-  )
+  let item =await authorizationUtils.itemPopulateAndAutorizationUse(itemId,req.userData.userId,next)
   item.itemTitle = newItemTitle
   try {
     const sess = await mongoose.startSession()
@@ -106,11 +75,7 @@ exports.deleteItem = async (req, res, next) => {
     )
   }
   const itemId = req.params.itemId
-  let item = await authorizationUtils.itemPopulateAndAutorizationUse(
-    itemId,
-    req.userData.userId,
-    next
-  )
+  let item =await authorizationUtils.itemPopulateAndAutorizationUse(itemId,req.userData.userId,next)
   try {
     const sess = await mongoose.startSession()
     sess.startTransaction()
